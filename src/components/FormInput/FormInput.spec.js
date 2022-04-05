89b195a68b421bd59ca5622f034c34a582ebf33d
Zeshan Shahid <98495821+ZeshanShahid79@users.noreply.github.com>
2022-04-05 12:43:43 +0200

    rework forminput spec

--- src/components/FormInput/FormInput.spec.js
+++ src/components/FormInput/FormInput.spec.js
@@ -3,6 +3,7 @@ import FormInput from './FormInput';
 
 describe('FormInput', () => {
   it('renders a label and input with Name of input and a span with a conditional errormesasage', () => {
+    const handleChange = jest.fn();
     render(
       <FormInput
         id="1"
@@ -12,11 +13,36 @@ describe('FormInput', () => {
         label="restaurantname"
         required={true}
         value="pattern"
+        onChange={handleChange}
       />
     );
 
     const nameOfContactFormInput = screen.getByLabelText(/restaurantname/i);
+    const errorSpan = screen.getByText(
+      /Should not include any special characters/i
+    );
 
     expect(nameOfContactFormInput).toBeInTheDocument();
+    expect(errorSpan).toBeInTheDocument();
+  });
+
+  it('renders a section', () => {
+    const handleChange = jest.fn();
+    render(
+      <FormInput
+        id="1"
+        name="Restaurantname"
+        type="text"
+        errorMessage="Should not include any special characters"
+        label="restaurantname"
+        required={true}
+        value="pattern"
+        onChange={handleChange}
+      />
+    );
+
+    const section = screen.getByRole('region', { name: 'restaurantname' });
+
+    expect(section).toBeInTheDocument();
   });
 });
